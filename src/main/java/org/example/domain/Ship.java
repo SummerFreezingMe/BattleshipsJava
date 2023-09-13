fc69901fd220a6241a3ee6d3f3bf1890b66d53e7
79969 <m.byckoff2015@yandex.ru>
2023-09-13 17:34:35 +0300

    feat: add fields and static factory to ship enum

--- src/main/java/org/example/domain/Ship.java
+++ src/main/java/org/example/domain/Ship.java
@@ -1,19 +1,38 @@
 package org.example.domain;
 
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+
 public enum Ship {
-    BATTLESHIP(4,1),
-    CRUISER(3,2),
-    DESTROYER(2,3),
-    SPEEDBOAT(1,4);
+    BATTLESHIP(4, 1),
+    CRUISER(3, 2),
+    DESTROYER(2, 3),
+    SPEEDBOAT(1, 4);
     private final int length;
     private final int amount;
+    @Setter
+    private Square left;
+    @Setter
+    private Square right;
 
     Ship(int length, int amount) {
-        this.length=length;
-        this.amount=amount;
+        this.length = length;
+        this.amount = amount;
 
+    }
+
+    public static List<Ship> listOfShips() {
+        List<Ship> fleet = new ArrayList<>();
+        for (Ship ship : Ship.class.getEnumConstants()) {
+            for (int i = 0; i < ship.amount; i++) {
+                fleet.add(ship);
+            }
+        }
+        return fleet;
     }
 }
