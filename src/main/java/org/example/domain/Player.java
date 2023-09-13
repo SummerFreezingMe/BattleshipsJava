8cd976766affa98c934c3998d4ae26608b6aeba3
79969 <m.byckoff2015@yandex.ru>
2023-09-13 14:17:07 +0300

    feat: add equals, toString and hashCode methods to Player

--- src/main/java/org/example/domain/Player.java
+++ src/main/java/org/example/domain/Player.java
@@ -2,9 +2,40 @@ package org.example.domain;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 public class Player {
     String name;
     boolean isAutoFill;
     boolean isBot;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        if (isAutoFill != player.isAutoFill) return false;
+        if (isBot != player.isBot) return false;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (isAutoFill ? 1 : 0);
+        result = 31 * result + (isBot ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", isAutoFill=" + isAutoFill +
+                ", isBot=" + isBot +
+                '}';
+    }
 }
